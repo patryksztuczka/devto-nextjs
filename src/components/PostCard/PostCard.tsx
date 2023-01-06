e1e5d079d567018fdf7631a8f478f058921e09d1
Patryk Sztuczka <patryk.sztuczka00@gmail.com>
2023-01-06 22:08:25 +0100

    Changed styling and added redirections

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -6,28 +6,37 @@ import profileImagePlaceholder from "../../assets/images/profile-image-placehold
 import HeartIcon from "../../assets/icons/HeartIcon";
 import CommentIcon from "../../assets/icons/CommentIcon";
 import BookmarkIcon from "../../assets/icons/BookmarkIcon";
+import Link from "next/link";
 
 const PostCard = ({ post }: IPostCardProps) => {
   const { title, author, createdAt } = post;
-  const { name, image } = author;
+  const { name, image, id } = author;
 
   return (
     <div className="flex w-full flex-col gap-2 bg-white p-4 shadow">
       <div className="flex items-center gap-2">
-        <Image
-          src={image || profileImagePlaceholder}
-          alt="Profile picture"
-          width={32}
-          height={32}
-          className="h-8 w-8 rounded-full"
-        />
+        <Link href={`/${id}`}>
+          <Image
+            src={image || profileImagePlaceholder}
+            alt="Profile picture"
+            width={32}
+            height={32}
+            className="h-8 w-8 rounded-full"
+          />
+        </Link>
         <div className="flex flex-col">
-          <h3 className="text-sm font-medium">{name}</h3>
-          <p className="text-xs">{createdAt.toString()}</p>
+          <Link href={`/${id}`}>
+            <h3 className="text-sm font-medium text-gray-700 hover:text-black">
+              {name}
+            </h3>
+          </Link>
+          <p className="text-xs text-gray-700">{createdAt.toString()}</p>
         </div>
       </div>
       <main>
-        <h1 className="text-lg font-bold">{title}</h1>
+        <h1 className="w-fit cursor-pointer text-lg font-bold hover:text-blue-800">
+          {title}
+        </h1>
       </main>
       <div className="flex justify-between gap-2">
         <div className="flex gap-2">
